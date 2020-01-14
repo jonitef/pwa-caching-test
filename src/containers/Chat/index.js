946a71f62fa2aae7b8f3388f476485297c356502
Joni Tefke <fi001450702@jonis-mbp.nhb-gb.ibmmobiledemo.com>
2020-01-14 08:56:58 +0200

    add/test background sync

--- src/containers/Chat/index.js
+++ src/containers/Chat/index.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 
 const styles = () => ({
     root: {
@@ -29,21 +30,58 @@ class Chat extends React.Component {
         this.props.callbackFromParent()
     };
 
+    submit = async (id) => {
+
+        let body = {
+            id: id,
+            wins: 20,
+            losses: 31,
+            points_scored: 10
+        }
+
+        let headers = {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+
+        let msg = {
+            'body': body,
+            'headers': headers
+        }
+
+        navigator.serviceWorker.controller.postMessage(msg)
+
+        console.log('submit')
+        fetch('https://back-opinnaytetyo.herokuapp.com/api/v1/stats', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: headers
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    };
+
     render() {
         const { classes } = this.props;
 
         return (
-
-            <AppBar position='static'>
-                <Toolbar>
-                    <IconButton edge="start" onClick={this.toggleDrawer}>
-                        <MenuIcon style={{ color: 'white' }} />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Chat
+            <div>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <IconButton edge="start" onClick={this.toggleDrawer}>
+                            <MenuIcon style={{ color: 'white' }} />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Chat
                     </Typography>
-                </Toolbar>
-            </AppBar>
+                    </Toolbar>
+                </AppBar>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                    <Button onClick={() => { this.submit(1001) }}>1001</Button>
+                    <Button onClick={() => { this.submit(1002) }}>1002</Button>
+                    <Button onClick={() => { this.submit(1003) }}>1003</Button>
+                </div>
+            </div>
+
 
         );
     }
--- src/sw-epilog.js
+++ src/sw-epilog.js
@@ -1,3 +1,87 @@
+openDatabase = () => {
+    let indexedDBOpenRequest = indexedDB.open('test-idb', 1)
+    indexedDBOpenRequest.onerror = (error) => {
+        console.error('IndexedDB error:', error)
+    }
+    indexedDBOpenRequest.onupgradeneeded = (e) => {
+        e.target.result.createObjectStore('post_requests', {
+            autoIncrement: true, keyPath: 'mytestidbid'
+        })
+    }
+    indexedDBOpenRequest.onsuccess = (e) => {
+        our_db = e.target.result
+    }
+}
+
+let our_db
+openDatabase()
+
+let body
+let headers
+let data
+
+getObjectStore = (storeName, mode) => {
+    return our_db.transaction(storeName, mode).objectStore(storeName)
+}
+
+savePostRequests = (url, payload) => {
+    let request = getObjectStore('post_requests', 'readwrite').add({
+        url: url,
+        payload: payload,
+        method: 'POST'
+    })
+    request.onsuccess = (event) => {
+        console.log('a new post_request has been added to indexedb')
+    }
+    request.onerror = (error) => {
+        console.error(error)
+    }
+}
+
+getPostResponse = (req) => {
+    return fetch(req.clone()).catch((error) => {
+        console.log(error)
+        console.log(req.clone())
+        savePostRequests(req.clone().url, data)
+    })
+};
+
+sendPostToServer = () => {
+    console.log('send post to server')
+    let savedRequests = []
+    let req = getObjectStore('post_requests').openCursor()
+    req.onsuccess = async (event) => {
+        let cursor = event.target.result
+        if (cursor) {
+            savedRequests.push(cursor.value)
+            cursor.continue()
+        } else {
+            for (let savedRequest of savedRequests) {
+                console.log('saved request', savedRequest)
+                let requestUrl = savedRequest.url
+                let payload = JSON.stringify(savedRequest.payload.body)
+                let method = savedRequest.method
+                let headers = savedRequest.payload.headers
+
+                fetch(requestUrl, {
+                    headers: headers,
+                    method: method,
+                    body: payload
+                }).then((response) => {
+                    console.log('server response', response.body)
+                    if (response.status < 400) {
+                        getObjectStore('post_requests',
+                            'readwrite').delete(savedRequest.mytestidbid)
+                    }
+                }).catch((error) => {
+                    console.error('Send to Server failed:', error)
+                    throw error
+                })
+            }
+        }
+    }
+}
+
 self.addEventListener('install', event => {
     console.log('install');
 });
@@ -5,4 +89,34 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     console.log('in activate')
     event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('message', (event) => {
+    console.log('data', event.data)
+    if (event.data.hasOwnProperty('body') && event.data.hasOwnProperty('headers')) {
+        body = event.data.body
+        headers = event.data.headers
+        data = event.data
+    }
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method === 'GET') {
+        console.log('GET')
+    }
+    if (event.request.method === 'POST') {
+        console.log(body)
+        console.log(headers)
+        event.respondWith(
+            getPostResponse(event.request.clone())
+        )
+    }
+});
+
+self.addEventListener('sync', (event) => {
+    console.log('in sync online')
+    if (event.tag === 'sync-tag') {
+        console.log('sync tag sendData')
+        event.waitUntil(sendPostToServer())
+    }
 });
